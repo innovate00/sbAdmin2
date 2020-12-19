6db79117a170c2228ce8171b1393ffaa919187fc
innovate00 <innovate00@naver.com>
2020-12-19 20:40:00 +0900

    Cognito 적용..

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_SEjnvMuQu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2eu93sca08umcev9v67s70g6bp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://8tyw07a6k9.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
