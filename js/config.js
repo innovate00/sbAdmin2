a67673eaa9081d648717c994bae378bf062b9272
innovate00 <innovate00@naver.com>
2020-12-20 18:56:10 +0900

    update..

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_SEjnvMuQu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2eu93sca08umcev9v67s70g6bp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_KcFO1HYSz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '124v78efo97ejm2vvfqkkld7d4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://1bda1a08sk.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://cpl5gv3mak.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
